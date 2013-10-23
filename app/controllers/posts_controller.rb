9a76ef20f58c506c8775e50e74c3aa8371fe53a0
maros <ap.maros@gmail.com>
2013-10-23 18:25:40 +0100

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,35 +19,21 @@ class PostsController < ApplicationController
   end
 
   # GET /posts/1/edit
-  def edit
-  end
+ # def edit
+  #end
 
   # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-	@post.message = @post.message.reverse
-
+	@post.message.reverse!
+	
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: 'Message was successfully reversed.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
